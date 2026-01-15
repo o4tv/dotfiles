a94bc2a1588cce5b74ea2911f983832732701609
o4tv <ot4vi0silv4@gmail.com>
2026-01-15 19:57:49 -0300

    add myfunctions(temp)

--- modules/user/space/bash/default.nix
+++ modules/user/space/bash/default.nix
@@ -1,4 +1,21 @@
 { config, ... }:
+
+let
+  myFunctions = # coloque esse "''$ para nao dar erro
+    ''
+      v() { nvim "''${@:-.}"; }
+
+      flaket() {
+          if [ -z "$1" ]; then
+              echo "Erro: Por favor, especifique o nome do template."
+              echo "Uso: flaket <nome-do-template>"
+              return 1
+          fi
+
+          nix flake init -t "path:/home/otavio/dev/templates#$1"
+      }
+    '';
+in
 {
   imports = [
     ./aliases.nix
@@ -8,6 +25,6 @@
   programs.bash = {
     enable = true;
     shellAliases = config.myAliases;
-    # initExtra = myFunctions;
+    initExtra = myFunctions;
   };
 }
