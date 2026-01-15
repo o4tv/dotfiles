6586e909723bc27bc4ea9ad4c386f8492ce4d3f0
o4tv <ot4vi0silv4@gmail.com>
2026-01-15 19:10:36 -0300

    gitsign setup nixvim

--- modules/user/apps/nixvim/config/setups.nix
+++ modules/user/apps/nixvim/config/setups.nix
@@ -38,6 +38,80 @@
     };
   };
 
+  plugins.gitsigns = {
+    enable = true;
+    settings = {
+      on_attach.__raw = ''
+          function(bufnr)
+              local gitsigns = require('gitsigns')
+
+              local function map(mode, lhs, rhs, opts)
+                  opts = opts or {}
+                  opts.buffer = bufnr
+                  vim.keymap.set(mode, lhs, rhs, opts)
+              end
+
+              -- Navigation
+              map('n', ']c', function()
+                  if vim.wo.diff then
+                      vim.cmd.normal({ ']c', bang = true })
+                  else
+                      gitsigns.nav_hunk('next')
+                  end
+              end, { desc = 'próximo hunk' })
+
+              map('n', '[c', function()
+                  if vim.wo.diff then
+                      vim.cmd.normal({ '[c', bang = true })
+                  else
+                      gitsigns.nav_hunk('prev')
+                  end
+              end, { desc = 'hunk anterior' })
+
+              -- Actions
+              map('n', '<leader>gs', gitsigns.stage_hunk, { desc = 'Stage do hunk atual' })
+              map('n', '<leader>gr', gitsigns.reset_hunk, { desc = 'Reset do hunk atual' })
+
+              map('v', '<leader>gs', function()
+                  gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+              end, { desc = 'Stage do trecho selecionado' })
+
+              map('v', '<leader>gr', function()
+                  gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+              end, { desc = 'Reset do trecho selecionado' })
+
+              map('n', '<leader>gS', gitsigns.stage_buffer, { desc = 'Stage do buffer inteiro' })
+              map('n', '<leader>gR', gitsigns.reset_buffer, { desc = 'Reset do buffer inteiro' })
+              map('n', '<leader>gp', gitsigns.preview_hunk, { desc = 'Preview do hunk atual' })
+              map('n', '<leader>gi', gitsigns.preview_hunk_inline, { desc = 'Preview inline do hunk atual' })
+
+              map('n', '<leader>gb', function()
+                  gitsigns.blame_line({ full = true })
+              end, { desc = 'Blame completo da linha atual' })
+
+              map('n', '<leader>gd', gitsigns.diffthis, { desc = 'Diff do arquivo atual' })
+
+              map('n', '<leader>gD', function()
+                  gitsigns.diffthis('~')
+              end, { desc = 'Diff contra o commit anterior' })
+
+              map('n', '<leader>gQ', function()
+                  gitsigns.setqflist('all')
+              end, { desc = 'Enviar todos os hunks para a quickfix list' })
+
+              map('n', '<leader>gq', gitsigns.setqflist, { desc = 'Enviar hunks do buffer para a quickfix list' })
+
+              -- Toggles
+              map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'Ativar/desativar blame inline' })
+              map('n', '<leader>td', gitsigns.toggle_word_diff, { desc = 'Ativar/desativar diff por palavra' })
+
+              -- Text object
+              map({ 'o', 'x' }, 'ih', gitsigns.select_hunk, { desc = 'Selecionar hunk como text object' })
+          end
+      '';
+    };
+  };
+
   plugins.barbar = {
     enable = true;
     settings = {
