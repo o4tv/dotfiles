e376df22ce508e5846ea36668987923c3ea29f98
o4tv <ot4vi0silv4@gmail.com>
2026-01-15 14:21:49 -0300

    cleaning

--- flake.nix
+++ flake.nix
@@ -9,11 +9,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # neovim-nightly-overlay = {
-    #     url = "github:nix-community/neovim-nightly-overlay";
-    # };
-
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
