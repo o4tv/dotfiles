bfc5cdb1ec1f8cf1822be368fc2841cd4db9deca
o4tv <ot4vi0silv4@gmail.com>
2026-01-19 16:48:02 -0300

    add vue js lsp support nixvim

--- modules/user/apps/nixvim/backstage/lsp.nix
+++ modules/user/apps/nixvim/backstage/lsp.nix
@@ -1,12 +1,36 @@
+{ lib, pkgs, ... }:
 {
-  plugins.lsp.enable = true;
-  lsp.servers = {
-    nil_ls.enable = true;
-    lua_ls.enable = true;
-    bashls.enable = true;
-    pyright.enable = true;
-    ts_ls.enable = true;
-    html.enable = true;
+  # dependencias
+  home.packages = with pkgs; [
+    vue-language-server
+  ];
+
+  plugins.lsp = {
+    enable = true;
+    servers = {
+      nil_ls.enable = true;
+      lua_ls.enable = true;
+      bashls.enable = true;
+      pyright.enable = true;
+      html.enable = true;
+      vue_ls.enable = true;
+    };
+  };
+
+  plugins.lsp.servers.ts_ls = {
+    enable = true;
+    filetypes = [ "vue" ];
+    extraOptions = {
+      init_options = {
+        plugins = [
+          {
+            name = "@vue/typescript-plugin";
+            location = "${lib.getBin pkgs.vue-language-server}/lib/language-tools/packages/language-server";
+            languages = [ "vue" ];
+          }
+        ];
+      };
+    };
   };
 
   plugins.fidget.enable = true;
