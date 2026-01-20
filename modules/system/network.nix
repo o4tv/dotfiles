fc5f39cfcdb6a119f5c5ec83b42e8d91a5c62077
o4tv <ot4vi0silv4@gmail.com>
2026-01-20 09:56:46 -0300

    allow vite on firewall

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -1,3 +1,8 @@
+let
+  allowedPorts = [
+    5173 # vite
+  ];
+in
 {
   networking.hostName = "dex";
 
@@ -5,6 +10,12 @@
   networking.wireless.networks."brisa-3388444" = {
     # Use 'wpa_passphrase SSID password' to generate this value
     pskRaw = "ea18dc8f85a8a44e648e9b98358ce0f8d6a0b1206cda0465a03bf85e66535840";
+  };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = allowedPorts;
+    allowedUDPPorts = allowedPorts;
   };
 
 }
