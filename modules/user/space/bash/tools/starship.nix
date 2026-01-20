52791d87c2120e845edb2facab5220c901652695
o4tv <ot4vi0silv4@gmail.com>
2026-01-20 18:40:01 -0300

    distrobox compatibility + colors starship

--- modules/user/space/bash/tools/starship.nix
+++ modules/user/space/bash/tools/starship.nix
@@ -6,11 +6,28 @@
     enable = true;
     settings = {
       nix_shell.symbol = "❄️ ";
-      directory.truncation_length = 8;
+      directory = {
+        style = "bold dimmed blue";
+        truncation_length = 8;
+      };
       character = {
         success_symbol = "[𖹬](purple)";
         error_symbol = "[✗](bold red)";
       };
+      container.disabled = true;
+      hostname = {
+        style = "bold dimmed red";
+        disabled = false;
+        ssh_only = false;
+      };
+      custom.distrobox = {
+        command = ''printf "%s" "$CONTAINER_ID"'';
+        when = ''test -n "$CONTAINER_ID"'';
+        symbol = "⬢";
+        format = "[$symbol ($output)]($style) on ";
+        style = "bold dimmed blue";
+      };
+      format = "$custom$all";
     };
     enableBashIntegration = true;
   };
