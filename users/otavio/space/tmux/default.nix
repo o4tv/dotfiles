e498a7e2a6d6d036566d9d05803119c7ac86875e
o4tv <ot4vi0silv4@gmail.com>
2026-01-22 02:44:15 -0300

    add tmux autostart on ssh session

--- users/otavio/space/tmux/default.nix
+++ users/otavio/space/tmux/default.nix
@@ -1,4 +1,6 @@
 {
+  programs.bash.profileExtra = ''[ -n "$SSH_CONNECTION" ] && [ -z "$TMUX" ] && (tmux attach -t main || tmux new -s main)'';
+
   programs.tmux = {
     enable = true;
     extraConfig = builtins.readFile ./tmux.conf;
