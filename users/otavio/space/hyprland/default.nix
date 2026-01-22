7e6f3b5c0db5335106bfa2d1dfaefd16602c561e
o4tv <ot4vi0silv4@gmail.com>
2026-01-22 01:54:04 -0300

    fix hyprland startup

--- users/otavio/space/hyprland/default.nix
+++ users/otavio/space/hyprland/default.nix
@@ -27,5 +27,5 @@
   '';
 
   # inicia hyprland sem dm somente no tty
-  programs.bash.profileExtra = ''[ "$CANT_RUN" == "1" ] &&  [ -z "$WAYLAND_DISPLAY" ] && [ -z "$DISPLAY" ] && uwsm check may-start 2>/dev/null && exec uwsm start hyprland.desktop'';
+  programs.bash.profileExtra = ''[ "$CANT_RUN" == "1" ] || [ -z "$WAYLAND_DISPLAY" ] && [ -z "$DISPLAY" ] && uwsm check may-start 2>/dev/null && exec uwsm start hyprland.desktop'';
 }
