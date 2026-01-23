1b51e16fa1d6ef623acc326b3006915310767fac
o4tv <ot4vi0silv4@gmail.com>
2026-01-23 13:33:08 -0300

    sway config

--- users/otavio/space/sway/default.nix
+++ users/otavio/space/sway/default.nix
@@ -1,31 +1,14 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 {
 
-  # home.packages = with pkgs; [
-  #   alacritty
-  #   rofi
-  #   hyprpaper
-  #   hyprpolkitagent
-  #   xdg-desktop-portal-hyprland
-  #   xdg-desktop-portal-gtk
-  #   wl-clipboard
-  #   hyprshot
-  #   hyprland
-  # ];
-  #
-  # services.hyprpaper.enable = true;
-  #
-  # home.file.".config/hypr/hyprland.conf".source = lib.mkForce ./hyprland.conf;
-  #
-  # home.file.".config/hypr/hyprpaper.conf".text = ''
-  #   splash = false
-  #   wallpaper {
-  #       monitor = 
-  #       path = ~/.wallpapers/tt.jpg
-  #       fit_mode = cover
-  #   }
-  # '';
-  #
-  # # inicia hyprland sem dm somente no tty
+  home.packages = with pkgs; [
+    alacritty
+    rofi
+    wl-clipboard
+  ];
+
+  home.file.".config/sway/config".source = ./config;
+
+  # inicia hyprland sem dm somente no tty
   # programs.bash.profileExtra = ''[ "$CANT_RUN" == "1" ] || [ -z "$WAYLAND_DISPLAY" ] && [ -z "$DISPLAY" ] && uwsm check may-start 2>/dev/null && exec uwsm start hyprland.desktop'';
 }
