a699686ac40cd3edadf06b024d8e0923857e948a
o4tv <ot4vi0silv4@gmail.com>
2026-03-01 17:44:17 -0300

    add bash completion

--- dot_config/bash/setups.sh
+++ dot_config/bash/setups.sh
@@ -1,2 +1,8 @@
+set show-all-if-ambiguous on
+set completion-ignore-case on
+
+[[ -f /usr/share/bash-completion/bash_completion ]] && \
+  . /usr/share/bash-completion/bash_completion
+
 eval "$(zoxide init bash --cmd cd)"
 eval "$(starship init bash)"
