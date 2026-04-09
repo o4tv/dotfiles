cd16486a48308b3c266d7ed6ac38b500762d96b4
o4tv <ot4vi0silv4@gmail.com>
2026-04-09 12:03:34 -0300

    setup pnpm

--- dot_config/bash/setups.sh
+++ dot_config/bash/setups.sh
@@ -3,5 +3,13 @@ export PATH="$PATH:$HOME/.local/bin"
 [[ -f /usr/share/bash-completion/bash_completion ]] && \
   . /usr/share/bash-completion/bash_completion
 
+# pnpm
+export PNPM_HOME="/home/tabin/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
 eval "$(zoxide init bash --cmd cd)"
 eval "$(starship init bash)"
